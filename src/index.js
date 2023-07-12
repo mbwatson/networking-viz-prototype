a1ff2dc56d8d500ccb961907992b6064bf94c4c5
Matt Watson <mwatson@renci.org>
2023-07-12 17:11:34 -0400

    swap browser-router for hash-router

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import { App } from "./app";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
 const ProvisionedApp = () => (
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
 
 root.render(<ProvisionedApp />);
